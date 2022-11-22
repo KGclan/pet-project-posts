772bbde2dc687a8f85bfb17ad0380f8b34a74513
Gorbenko_KV <gorbenko_kv@km-union.ru>
2022-11-22 21:40:12 +0300

    Добавил лоадер в компоненте списка постов

--- src/widgets/posts-list/index.tsx
+++ src/widgets/posts-list/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { CircularProgress } from '@mui/material';
 
 import { IPost } from 'shared/api';
 import { PostCard } from 'entities/post';
 
 import styles from './styles.module.scss';
+import { RootState } from 'app/store';
 
 interface IPostsList {
     posts: IPost[] | null;
 }
 
-const PostsList = ({
-    posts,
-}: IPostsList) => {
+const PostsList = ({ posts }: IPostsList) => {
+    const isLoading = useSelector((state: RootState) => state.posts.isListLoading);
+
+    if (posts === null) return <div>Данных нет</div>;
+
     return (
         <div className={styles.wrapper}>
             {
-                posts !== null
-                    ? posts.map((post, index) => <PostCard key={index} postData={post} />)
-                    : <div>Данных нет</div>
+                isLoading
+                    ? <CircularProgress />
+                    : posts.map((post, index) => <PostCard key={index} postData={post} />)
             }
         </div>
     );
